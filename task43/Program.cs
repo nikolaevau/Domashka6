0cc6d58936e66dca810a93cfbc883b9af8df0f60
Uliana <ashmishmish125@gmail.com>
2022-08-26 12:50:23 +0300

    task43

--- task43/Program.cs
+++ task43/Program.cs
@@ -16,8 +16,8 @@ Console.WriteLine("Введите значение k2");
 double k2 = Convert.ToDouble(Console.ReadLine());
 
 double x = (b2 - b1) / (k1 - k2);
-double y1 = k1 * x + b1;
-double y2 = k2 * x + b2;
+double y = k1 * x + b1;
+
 
 if ((k1 == k2) && (b1 == b2))
 
@@ -29,4 +29,4 @@ Console.WriteLine("Прямые параллельны");
 
 else
 
-Console.WriteLine($"Точка пересечения прямых ({y1:f1}; {y2:f1})");
+Console.WriteLine($"Точка пересечения прямых ({y:f1}; {x:f1})");
